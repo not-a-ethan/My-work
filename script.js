6dbce9972d3d61e22ed5f25166dd215185a9c43d
Ethan <92686703+ethan-master-coding@users.noreply.github.com>
2022-05-08 09:24:08 -0400

    Fix to the change I just made.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let image = document.getElementById('image')
 
 function change() {
   if (image.getAttribute(href) = "Images/CC.png"){
-    image.setAttribute(href, Images/discord.png)
+    image.setAttribute("href", "Images/discord.png")
   }else{
-    image.setAttribute(href, Images/CC.png)
+    image.setAttribute("href", "Images/CC.png")
   }
 }
 
