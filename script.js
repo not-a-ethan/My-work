a09d219135862ae63de987f6b05107ece54af9cb
Ethan <92686703+ethan-master-coding@users.noreply.github.com>
2022-05-08 09:56:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let image = document.getElementById('image')
 
 function change() {
   if (image.getAttribute("src") == "Images/CC.png"){
-    image.src = "images/discord.png"
+    image.src = "Images/Discord.png"
   }else{
     image.src = "Images/CC.png"
   }
