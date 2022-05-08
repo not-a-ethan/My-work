964ac2ec021f5a9f39269ce86534772ecea0de21
Ethan <92686703+ethan-master-coding@users.noreply.github.com>
2022-05-08 10:01:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ let image = document.getElementById('image')
 function change() {
   if (image.getAttribute("src") == "Images/CC.png"){
     image.src = "Images/Discord.png"
+    image.alt = "Discord logo"
   }else{
     image.src = "Images/CC.png"
+    image.alt = "Codecademy logo"
   }
 }
 
