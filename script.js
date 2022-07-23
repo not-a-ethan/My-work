792e40a50878edd2e5e15794dc8547eaa66d47a0
Seymour Rodrigues <seymourjr5@gmail.com>
2022-07-23 13:47:13 +0100

    Navbar fixed

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ function change() {
 }
 
 image.onclick = change
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
